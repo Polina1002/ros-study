305d506c3adc8df344ba2b0a2037ed9d47425f87
Polina1002 <116268118+Polina1002@users.noreply.github.com>
2022-11-09 00:21:25 +0300

    Add files via upload

---
+++ polynomchek.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import rospy
+#from std_msgs.msg import Int64
+from study_pkg.msg import Control
+
+def callback(msg):
+    
+    serv = Control()
+    serv.x = msg.x
+    serv.y = msg.y ** 2
+    pub = rospy.Publisher('raschet', Control, queue_size=10)
+    pub.publish(serv)
+    rospy.loginfo("Polynomchek is = %d, %d", serv.x, serv.y)
+
+rospy.init_node('Polynomcheeek')
+rospy.Subscriber('vvod', Control, callback, queue_size=10)
+
+rospy.spin()
